0827dd31ce948104fbb440201c36f9d6c1944f3e
AlfaroE <esau.alfaro00@gmail.com>
2023-06-15 21:54:54 -0600

    Arreglo de bug

--- admin_utilities.py
+++ admin_utilities.py
@@ -17,30 +17,47 @@ def algo_request():
     r = requests.get('https://patents-1f7da-default-rtdb.firebaseio.com/data/algoRequests.json')
     if r.status_code == 200:
         dict = r.json()
-        for key in dict:
-            usrAddress = dict[key]['usrAddress']
-            amount = dict[key]['amount']
-            print('\t'+'id de solicitud= ' + key)
-            print('usrAddress= '+ usrAddress)
-            print('amount= '+ amount)
-            print('ticketBarCode= '+dict[key]['ticketBarCode'])
-            print('')
-            opt = input('\t Procesar Solicitud?    y/n \n r: ')
-            if opt == 'y':
-                address = input("Ingrese Address de su cuenta:\t")
-                mnemonic = input("Ingrese el memotécnico de su Private key:\t")
-                txid = transfer_Algos(address,usrAddress,mnemonic,(int(amount)*1000000))
-                opt2 = input('\t Se concreto la transacción?    y/n \n r: ')
-                if opt2 == 'y':
+        if len(dict) > 1:
+            for key in dict:
+                usrAddress = dict[key]['usrAddress']
+                amount = dict[key]['amount']
+                print('\t'+'id de solicitud= ' + key)
+                print('usrAddress= '+ usrAddress)
+                print('amount= '+ amount)
+                print('ticketBarCode= '+dict[key]['ticketBarCode'])
+                print('')
+                opt = input('\t Procesar Solicitud?    y/n \n r: ')
+                if opt == 'y':
+                    address = input("Ingrese Address de su cuenta:\t")
+                    mnemonic = input("Ingrese el memotécnico de su Private key:\t")
+                    txid = transfer_Algos(address,usrAddress,mnemonic,(int(amount)*1000000))
+                    opt2 = input('\t Se concreto la transacción?    y/n \n r: ')
+                    if opt2 == 'y':
+                        r2 = requests.delete('https://patents-1f7da-default-rtdb.firebaseio.com/data/algoRequests/'+key+'.json')
+                        if r2.status_code == 200:
+                            print('La transacción fue exitosa')
+                            if(((len(dict)) - 1) < 2):
+                                print('No hay más solicitudes')
+                                break
+                        else:
+                            print('La transacción no fue completada')
+                            if(((len(dict)) - 1) < 2):
+                                print('No hay más solicitudes')
+                                break
+                    elif opt2 == 'n':
+                        r2 = requests.delete('https://patents-1f7da-default-rtdb.firebaseio.com/data/algoRequests/'+key+'.json')
+                        print('La solicitud NO fue procesada y fue eliminada')
+                        if(((len(dict)) - 1) < 2):
+                            print('No hay más solicitudes')
+                            break
+                elif opt == 'n':
                     r2 = requests.delete('https://patents-1f7da-default-rtdb.firebaseio.com/data/algoRequests/'+key+'.json')
-                    if r2.status_code == 200:
-                        print('La transacción fue exitosa')
-                    else:
-                        print('La transacción no fue completada')
-                elif opt2 == 'n':
-                    print('La solicitud NO fue procesada')
-            elif opt == 'n':
-                print('La solicitud NO fue procesada')
+                    print('La solicitud NO fue procesada y fue eliminada')
+                    if(((len(dict)) - 1) < 2):
+                        print('No hay más solicitudes')
+                        break
+        else:
+            print('No hay más solicitudes')
     else:
         print('La solicitud fracaso')
 
@@ -48,37 +65,54 @@ def patent_request():
     r = requests.get('https://patents-1f7da-default-rtdb.firebaseio.com/data/patentRequests.json')
     if r.status_code == 200:
         dict = r.json()
-        for key in dict:
-            usrAddress = dict[key]['usrAddress']
-            patentTitle = dict[key]['patentTitle']
-            fileHash = dict[key]['fileHash']
-            filePath = dict[key]['filePath']
-            print('\t'+'id de solicitud= ' + key)
-            print('usrAddress= '+ usrAddress)
-            print('patentTtile= '+ patentTitle)
-            print('txid= '+ dict[key]['txid'])
-            print('fileHash= '+fileHash)
-            print('filePath= '+filePath)
-            print('')
-            opt = input('\t Procesar Solicitud?    y/n \n r: ')
-            if opt == 'y':
+        if len(dict) > 1:
+            for key in dict:
+                usrAddress = dict[key]['usrAddress']
+                patentTitle = dict[key]['patentTitle']
+                fileHash = dict[key]['fileHash']
+                filePath = dict[key]['filePath']
+                print('\t'+'id de solicitud= ' + key)
+                print('usrAddress= '+ usrAddress)
+                print('patentTtile= '+ patentTitle)
+                print('txid= '+ dict[key]['txid'])
+                print('fileHash= '+fileHash)
+                print('filePath= '+filePath)
                 print('')
-                mnemonic = input("Ingrese el memotécnico de Private key de Manager:\t")
-                transfer_assets(usrAddress,mnemonic,fileHash)
-                opt2 = input('\t Se concreto la transacción?    y/n \n r: ')
-                if opt2 == 'y':
-                    request_dict = {'file':filePath,'patentTitle':patentTitle}
-                    request_dict_json = json.dumps(request_dict)
-                    r2 = requests.post('https://patents-1f7da-default-rtdb.firebaseio.com/data/patentCatalog.json',data=request_dict_json)
+                opt = input('\t Procesar Solicitud?    y/n \n r: ')
+                if opt == 'y':
+                    print('')
+                    mnemonic = input("Ingrese el memotécnico de Private key de Manager:\t")
+                    transfer_assets(usrAddress,mnemonic,fileHash)
+                    opt2 = input('\t Se concreto la transacción?    y/n \n r: ')
+                    if opt2 == 'y':
+                        request_dict = {'file':filePath,'patentTitle':patentTitle}
+                        request_dict_json = json.dumps(request_dict)
+                        r2 = requests.post('https://patents-1f7da-default-rtdb.firebaseio.com/data/patentCatalog.json',data=request_dict_json)
+                        r3 = requests.delete('https://patents-1f7da-default-rtdb.firebaseio.com/data/patentRequests/'+key+'.json')
+                        if r3.status_code == 200 and r2.status_code == 200:
+                            print('La transacción fue exitosa')
+                            if(((len(dict)) - 1) < 2):
+                                print('No hay más solicitudes')
+                                break
+                        else:
+                            print('La transacción no fue completada')
+                            if(((len(dict)) - 1) < 2):
+                                print('No hay más solicitudes')
+                                break
+                    elif opt2 == 'n':
+                        r3 = requests.delete('https://patents-1f7da-default-rtdb.firebaseio.com/data/patentRequests/'+key+'.json')
+                        print('La solicitud NO fue procesada y fue eliminada')
+                        if(((len(dict)) - 1) < 2):
+                            print('No hay más solicitudes')
+                            break
+                elif opt == 'n':
                     r3 = requests.delete('https://patents-1f7da-default-rtdb.firebaseio.com/data/patentRequests/'+key+'.json')
-                    if r3.status_code == 200 and r2.status_code == 200:
-                        print('La transacción fue exitosa')
-                    else:
-                        print('La transacción no fue completada')
-                elif opt2 == 'n':
-                    print('La solicitud NO fue procesada')
-            elif opt == 'n':
-                print('La solicitud NO fue procesada')
+                    print('La solicitud NO fue procesada y fue eliminada')
+                    if(((len(dict)) - 1) < 2):
+                        print('No hay más solicitudes')
+                        break
+        else:
+            print('No hay más solicitudes')
     else:
         print('La solicitud fracaso')
    
